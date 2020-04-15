7fcda8cc92a50ef1abed960a61f3fb1288390b77
Lu Chang <chinaluchang@live.com>
2020-04-15 08:43:10 +0800

    Linux path style support

--- nga.py
+++ nga.py
@@ -54,7 +54,7 @@ def single(page):
 def makefile():
     global localmaxfloor
     lastfloor = 0
-    with open(('.\\\\%d\\post.md' % tid),'a',encoding='utf-8') as f:
+    with open(('./%d/post.md' % tid),'a',encoding='utf-8') as f:
         for onefloor in totalfloor:
             if localmaxfloor<int(onefloor[0]):
                 
@@ -71,14 +71,14 @@ def makefile():
                         url = 'https://img.nga.178.com/attachments/' + url[2:]
                     url = url.replace('.medium.jpg','')
                     filename = hashlib.md5(bytes(url, encoding='utf-8')).hexdigest()[2:8] + url[-6:]
-                    if os.path.exists('.\\\\%d\\%s' % (tid,filename)) == False:
-                        down(url,('.\\\\%d\\%s' % (tid,filename)))
-                        print('down:%s' % ('.\\\\%d\\%s' % (tid,filename)))
-                    raw = raw.replace(('[img]%s[/img]' % ritem),('![img](.\\\\%s)' % filename))
+                    if os.path.exists('./%d/%s' % (tid,filename)) == False:
+                        down(url,('./%d/%s' % (tid,filename)))
+                        print('down:%s' % ('./%d/%s' % (tid,filename)))
+                    raw = raw.replace(('[img]%s[/img]' % ritem),('![img](./%s)' % filename))
 
                 rex = re.findall(r'\[s\:(a2|ac)\:(.+?)\]',raw)#表情
                 for ritem in rex:
-                    raw = raw.replace('[s:%s:%s]' % (ritem[0], ritem[1]),'![%s](..\\\\smile\\%s.png)' % (ritem[0]+ritem[1],ritem[0]+ritem[1]))
+                    raw = raw.replace('[s:%s:%s]' % (ritem[0], ritem[1]),'![%s](../smile/%s.png)' % (ritem[0]+ritem[1],ritem[0]+ritem[1]))
                 #[0]人名 [1]时间 [2]圈的内容
                 rex = re.findall(r'\[quote\].+?\[uid=\d+\](.+?)\[/uid\] \((.+?)\)\:\[/b\](.+?)\[/quote\]',raw, flags=re.S)#引用 [quote][tid=0000000]Topic[/tid] [b]Post by [uid=000000]whowhowho[/uid] (2020-03-26 01:07):[/b]
                 for ritem in rex:
@@ -107,10 +107,10 @@ def holder():
     global localmaxpage
     global localmaxfloor
     print(tid)
-    if not os.path.exists(('.\\\\%d' % tid)):
-        os.mkdir(('.\\\\%d' % tid))
-    elif os.path.exists('.\\\\%d\\max.txt' % tid):
-        with open('.\\\\%d\\max.txt' % tid,'r',encoding='utf-8') as f:
+    if not os.path.exists(('./%d' % tid)):
+        os.mkdir(('./%d' % tid))
+    elif os.path.exists('./%d/max.txt' % tid):
+        with open('./%d/max.txt' % tid,'r',encoding='utf-8') as f:
             r = f.read()
             localmaxpage = int(r.split( )[0])
             localmaxfloor = int(r.split( )[1])
@@ -120,14 +120,14 @@ def holder():
     while single(cpage) != False:
         cpage = cpage + 1
 
-    with open(('.\\\\%d\\max.txt' % tid),'w',encoding='utf-8') as f:
+    with open(('./%d/max.txt' % tid),'w',encoding='utf-8') as f:
         f.write("%d %s" % (cpage, totalfloor[len(totalfloor) - 1][0]))
     
-    if os.path.exists('.\\\\%d\\info.txt' % tid):
-        with open(('.\\\\%d\\info.txt' % tid),'a',encoding='utf-8') as f:
+    if os.path.exists('./%d/info.txt' % tid):
+        with open(('./%d/info.txt' % tid),'a',encoding='utf-8') as f:
             f.write('[%s]%d\n' % (time.asctime(time.localtime(time.time())), len(totalfloor) - 1))
     else:
-        with open(('.\\\\%d\\info.txt' % tid),'w',encoding='utf-8') as f:
+        with open(('./%d/info.txt' % tid),'w',encoding='utf-8') as f:
             f.write('tid:%d\n' % tid)
             f.write(('[%s]%d\n' % (time.asctime(time.localtime(time.time())), len(totalfloor) - 1)))
     
